089a4b22cf93fb6a324e0b61bf4ecd036021f484
parasjain1 <parasjain.jain@outlook.com>
2019-08-18 14:58:22 +0530

    new test commit

--- app/core/models.py
+++ app/core/models.py
@@ -29,7 +29,7 @@ class UserManager(BaseUserManager):
 class User(AbstractBaseUser, PermissionsMixin):
 	"""Custom user model that supporsts using email instead of username"""
 	email = models.EmailField(max_length=255, unique=True)
-	name = models.CharField(max_length=255)
+	name = models.CharField(max_length=250)
 	is_active = models.BooleanField(default=True)
 	is_staff = models.BooleanField(default=False)
 	objects = UserManager()
