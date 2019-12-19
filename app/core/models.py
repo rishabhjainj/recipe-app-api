2a2e1bb040b0ee3a5787455add5191ca530a2750
parasjain1 <parasjain.jain@outlook.com>
2019-12-19 13:39:28 +0530

    db wait command added.

--- app/core/models.py
+++ app/core/models.py
@@ -12,7 +12,7 @@ class UserManager(BaseUserManager):
 			raise ValueError("Users must have an email address")
 		user = self.model(email=self.normalize_email(email), **extra_fields)
 		user.set_password(password)
-		user.save(using=self._db);
+		user.save(using=self._db)
 
 		return user
 
--- app/user/serializers.py
+++ app/user/serializers.py
@@ -1,5 +1,6 @@
-from django.contrib.auth import get_user_model
+from django.contrib.auth import get_user_model, authenticate
 from rest_framework import serializers
+from django.utils.translation import ugettext_lazy as _
 
 
 class UserSerializer(serializers.ModelSerializer):
@@ -8,8 +9,32 @@ class UserSerializer(serializers.ModelSerializer):
 	class Meta:
 		model = get_user_model()
 		fields = ('email', 'password', 'name')
-		extra_kwargs = {'password': {'write_only': True, 'min_length':5}}
+		extra_kwargs = {'password': {'write_only': True, 'min_length': 5}}
 
 	def create(self, validated_data):
 		"""Create a new user with encrypted password and return it"""
 		return get_user_model().objects.create_user(**validated_data)
+
+
+class MyAuthTokenSerializer(serializers.Serializer):
+		"""Serial for the user authentication object"""
+		email = serializers.CharField()
+		password = serializers.CharField(
+			style={'input_type': 'password'},
+			trim_whitespace=False
+		)
+
+		def validate(self, attrs):
+			"""Validate and authenticate the user"""
+			email = attrs.get('email')
+			password = attrs.get('password')
+			user = authenticate(
+				request=self.context.get('request'),
+				email=email,
+				password=password
+			)
+			if not user:
+				msg = _('Unable to authenticate with provided credentials')
+				raise serializers.ValidationError(msg, code='authentication')
+			attrs['user'] = user
+			return attrs
